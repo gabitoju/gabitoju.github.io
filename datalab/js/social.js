012c8b20185d7393d6901953f6f534bd0e326234
Juan Gabito <gabitoju@gmail.com>
2014-06-18 12:12:28 -0300

    Social loading times

--- datalab/js/social.js
+++ datalab/js/social.js
@@ -12,6 +12,6 @@ window.___gcfg = {lang: 'es-419'};
 
 (function() {
 var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-po.src = 'https://apis.google.com/js/platform.js';
+po.src = '//apis.google.com/js/platform.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
